def7a2a64f09d81e21ca8aabe9fb0c92bfe67960
IT21163586 <99707489+IT21163586@users.noreply.github.com>
2022-02-16 18:18:09 +0000

    tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+  int sub1,sub2;
+  float sum,average;
+
+  printf("Enter the sub1 Mark :");
+  scanf("%d",&sub1);
+
+  printf("Enter the sub2 Mark :");
+  scanf("%d",&sub2);
+
+  sum=sub1+sub2;
+  average=sum/2.0;
+  printf("Average = %.2f",average);
+
+
   
   return 0;
 }
--- Tute03.c
+++ Tute03.c
@@ -8,11 +8,21 @@ e.g.
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
-n -> 1-
+n -> 10
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int n, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
   
   return 0;
 }
